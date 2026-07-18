a4b603c79aa41b6087c083d49a97902ea77486c4
mAru01978 <maru1015928@gmail.com>
2026-07-19 02:31:49 +0900

    Update

--- scripts/build-vnlayer-standalone.js
+++ scripts/build-vnlayer-standalone.js
@@ -9,11 +9,12 @@
 
 const esbuild = require('esbuild');
 const path = require('path');
-
+const root = path.resolve(__dirname, "..");
 esbuild
   .build({
-    entryPoints: [path.join(__dirname, '..',"..",'VNLayer', 'standalone.ts')],
-    outfile: path.join(__dirname, '..', 'dist', 'vnlayer.js'),
+    absWorkingDir: root,
+    entryPoints: [path.join(root, 'standalone.ts')],
+    outfile: path.join(root, 'dist', 'vnlayer.js'),
     bundle: true,
     minify: true,
     format: 'iife',
